d908d08751c155f39dc5f0692fbc653fedc16dcc
GabrielCamarillo <156870835+GabrielCamarillo@users.noreply.github.com>
2024-01-23 19:12:28 -0600

    Upload Homework Question 1

---
+++ Homework2Q1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // User Input Grade
+        Console.Write("Please input a letter grade:");
+        string inputLetter = Console.ReadLine().ToUpper();
+
+        // Determine GPA Point
+        int gpaPoint;
+        switch (inputLetter)
+    {
+            case "A":
+                gpaPoint = 4;
+                break;
+            case "B":
+                gpaPoint = 3;
+                break;
+            case "C":
+                gpaPoint = 2;
+                break;
+            case "D":
+                gpaPoint = 1;
+                break;
+            case "F":
+                gpaPoint = 0;
+                break;
+            default:
+                Console.WriteLine("Wrong Letter Grade!");
+                return;
+        }
+
+        // Final GPA Point
+        Console.WriteLine($"GPA Point {inputLetter}: {gpaPoint}");
+
+        // ChatGPT. 2023, Jan 23. "It's not switching/reading my letter correctly how can i fix it"
+    }
+} 
